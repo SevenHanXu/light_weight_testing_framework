39c45235be2184ac73d4602ea52adecdf242b679
HanXu <601042507@qq.com>
2018-09-25 13:28:58 +0800

    增加到头文件宏定义

--- binary_search.c
+++ binary_search.c
@@ -5,11 +5,9 @@
 	> Created Time: 二  9/25 10:19:55 2018
  ************************************************************************/
 
-#include<stdio.h>
-
-#define TEST(a, b){\
-    printf("%s == %s : %s\n", #a, #b, (a) == (b) ? "OK" : "FAIL");\
-}
+#include <stdio.h>
+#include <assert.h>
+#include "test.h"
 
 int binary_search(int *num, int x, int len){
     int head = 0, tail = len - 1, mid;
@@ -23,15 +21,16 @@ int binary_search(int *num, int x, int len){
 }
 
 
+
 int main(){
     int num[10] = {0};
     for(int i = 0; i < 10; i++){
         num[i] = i + 1;
     }
-    TEST(binary_search(num, -1, 10), -1);
-    TEST(binary_search(num, 11, 10), -1);
-    TEST(binary_search(num, 2, 10), 1);
-    TEST(binary_search(num, 1, 10), 0);
-    TEST(binary_search(num, 10, 10), 9);
+    ASSERT_EQ(binary_search(num, -1, 10), -1);
+    ASSERT_EQ(binary_search(num, 11, 10), -1);
+    ASSERT_EQ(binary_search(num, 2, 10), 1);
+    ASSERT_EQ(binary_search(num, 1, 10), 0);
+    ASSERT_EQ(binary_search(num, 10, 10), 9);
     return 0;
 }
